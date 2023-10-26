dccdd2fa03a615ad8fcbb8a2507edff621e660b7
Gabriel R. Antunes <gabrielrodantunes@gmail.com>
2023-10-25 20:01:20 -0400

    feat: init development workspace

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,10 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  const port = process.env.PORT ?? 3013;
+
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  await app.listen(port);
 }
 bootstrap();
